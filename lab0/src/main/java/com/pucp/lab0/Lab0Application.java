26ceba1b1d0edfbce07e988a80131647c50b11bf
oshixmi <oshin.ccaico@pucp.edu.pe>
2025-08-21 21:00:38 -0500

    primer commit

---
+++ lab0/src/main/java/com/pucp/lab0/Lab0Application.java
@@ -0,0 +1,13 @@
+package com.pucp.lab0;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Lab0Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Lab0Application.class, args);
+	}
+
+}
---
+++ lab0/src/test/java/com/pucp/lab0/Lab0ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pucp.lab0;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Lab0ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
